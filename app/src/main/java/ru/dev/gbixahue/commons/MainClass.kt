e84f44460e8aefc87d0b67cd8b14d35d4a7fcb7a
Zhilenkov <gbixahue@gmail.com>
2018-06-14 20:31:41 +0300

    trying to fix

---
+++ app/src/main/java/ru/dev/gbixahue/commons/MainClass.kt
@@ -0,0 +1,10 @@
+package ru.dev.gbixahue.commons
+
+/**
+ * Created by Anton Zhilenkov on 14.06.2018.
+ */
+class MainClass {
+
+  fun cl(){
+  }
+}
---
+++ library/src/main/java/ru/dev/gbixahue/library/SomeClass.kt
@@ -0,0 +1,10 @@
+package ru.dev.gbixahue.library
+
+/**
+ * Created by Anton Zhilenkov on 14.06.2018.
+ */
+class SomeClass {
+  companion object {
+    fun openTool(name: String): String = "sajdnfg"
+  }
+}
